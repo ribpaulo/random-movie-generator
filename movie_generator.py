6417b6b308d074350643db00779823f9e4352c42
Paulo <n.rib.paulo@gmail.com>
2023-02-17 20:56:15 +0100

    add import random and select random movie

--- movie_generator.py
+++ movie_generator.py
@@ -1,4 +1,10 @@
 # Random Movie Generator v1.0
 
+import random
+
 movieList = ["Black Panther: Wakanda Forever", "Babylon",
              "Avatar: The Way of Water", "The Menu", "The Banshees of Inisherin"]
+
+random_movie = random.choice(movieList)
+
+print(random_movie)
